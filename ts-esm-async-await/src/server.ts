0cf487ba355336428c4becebf689cebec94218ec
Ifycode <marychord@gmail.com>
2024-04-02 09:27:58 +0100

    Rename boilerplates to reflect syntax

--- ts-esm-async-await/src/server.ts
+++ ts-esm-async-await/src/server.ts
@@ -1,6 +1,6 @@
 import mongooseConnect from './db.connect';
 import { app as app } from './app';
-import { connectionType } from '../node-mongo-helpers';
+import { connectionType } from './node-mongo-helpers';
 
 
 const port = connectionType().port || 3000;
