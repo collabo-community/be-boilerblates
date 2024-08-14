1de4dcaf2aa7e55c9ee647b201b0cd23e3122e36
samuko-things <samuel.c.agba@gmail.com>
2024-08-14 18:07:23 +0100

    added and configured passport strategies - local, jwt, google, github

---
+++ ts-esm-async-await/src/api/passport-auth-config/strategies/local.login.strategy.ts
@@ -0,0 +1,27 @@
+import {Strategy} from 'passport-local';
+import { UserModel as User } from '../../models/user.model';
+import bcrypt from 'bcrypt';
+import { DoneCallback } from 'passport';
+import { badRequestErr } from '../../../lib/errors/Errors';
+
+export const localLoginStrategy = new Strategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  async (email: string, password: string, done: DoneCallback)=>{
+    try {
+      const user = await User.findOne({email: email}).exec();
+      if (!user) {
+        badRequestErr("Invalid Credentials [email]")
+      }
+      const passwordMatches = await bcrypt.compare(password, user.password);
+      if (!passwordMatches) {
+        badRequestErr("Invalid Credentials [password]")
+      }
+      done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+);
